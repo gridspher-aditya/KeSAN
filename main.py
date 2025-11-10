e7ac68dc21c9617f7498b8350c76968c5f2dab8a
buildwithadi <adityarawat.online@gmail.com>
2025-11-10 12:26:25 +0530

    CORS updated

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173","http://localhost:3000/station/kisan"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
